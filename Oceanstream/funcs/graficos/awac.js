6c725e1f9bf1253db5a89f73c675916978c8b45d
Giuliano Demoner <giuliano.demoner@gmail.com>
2025-09-16 12:07:20 -0300

    Adicionado página Política + correção timestamp

--- Oceanstream/funcs/graficos/awac.js
+++ Oceanstream/funcs/graficos/awac.js
@@ -49,6 +49,19 @@ document.addEventListener('DOMContentLoaded', async function() {
             tooltips: {
                 mode: 'index',
                 intersect: false,
+                callbacks: {
+                    title: function(tooltipItems, data) {
+                        const date = new Date(tooltipItems[0].xLabel);
+                        const year = date.getFullYear();
+                        const month = String(date.getMonth() + 1).padStart(2, '0');
+                        const day = String(date.getDate()).padStart(2, '0');
+                        const hours = String(date.getHours()).padStart(2, '0');
+                        const minutes = String(date.getMinutes()).padStart(2, '0');
+
+                        return `${day}/${month}/${year} - ${hours}:${minutes}`; // dd/mm/yyyy hh:mm
+                        // return `${year}/${month}/${day} ${hours}:${minutes}`; // yyyy/mm/dd hh:mm
+                    }
+                }
             },
             hover: {
                 mode: 'nearest',
@@ -121,6 +134,19 @@ document.addEventListener('DOMContentLoaded', async function() {
             tooltips: {
                 mode: 'index',
                 intersect: false,
+                callbacks: {
+                    title: function(tooltipItems, data) {
+                        const date = new Date(tooltipItems[0].xLabel);
+                        const year = date.getFullYear();
+                        const month = String(date.getMonth() + 1).padStart(2, '0');
+                        const day = String(date.getDate()).padStart(2, '0');
+                        const hours = String(date.getHours()).padStart(2, '0');
+                        const minutes = String(date.getMinutes()).padStart(2, '0');
+
+                        return `${day}/${month}/${year} - ${hours}:${minutes}`; // dd/mm/yyyy hh:mm
+                        // return `${year}/${month}/${day} ${hours}:${minutes}`; // yyyy/mm/dd hh:mm
+                    }
+                }
             },
             hover: {
                 mode: 'nearest',
@@ -186,6 +212,19 @@ document.addEventListener('DOMContentLoaded', async function() {
             tooltips: {
                 mode: 'index',
                 intersect: false,
+                callbacks: {
+                    title: function(tooltipItems, data) {
+                        const date = new Date(tooltipItems[0].xLabel);
+                        const year = date.getFullYear();
+                        const month = String(date.getMonth() + 1).padStart(2, '0');
+                        const day = String(date.getDate()).padStart(2, '0');
+                        const hours = String(date.getHours()).padStart(2, '0');
+                        const minutes = String(date.getMinutes()).padStart(2, '0');
+
+                        return `${day}/${month}/${year} - ${hours}:${minutes}`; // dd/mm/yyyy hh:mm
+                        // return `${year}/${month}/${day} ${hours}:${minutes}`; // yyyy/mm/dd hh:mm
+                    }
+                }
             },
             hover: {
                 mode: 'nearest',
